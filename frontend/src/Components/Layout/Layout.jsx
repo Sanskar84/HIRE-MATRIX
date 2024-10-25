be6da539b0f268c0e1b7914ebaf660657e9baecd
Sanskar84 <bharadiasanskar@gmail.com>
2024-10-25 14:46:00 +0530

    created test route result route and some error handling

--- frontend/src/Components/Layout/Layout.jsx
+++ frontend/src/Components/Layout/Layout.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import styles from './Layout.module.scss'
 const Layout = () => {
   return (
-    <div>Layout</div>
-  )
+    <div>
+    <div></div>
+  </div>
+)
 }
-export default Layout
