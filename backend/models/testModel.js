c98741f12f562e3967a3d1a7f68da3610b6ea932
vaibhavmani810 <vaibhavmani810@gmail.com>
2024-11-05 00:37:56 +0530

    Created test Model

--- backend/models/testModel.js
+++ backend/models/testModel.js
@@ -2,88 +2,89 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const testSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please enter a Test name'],
-      },
-      email: {
-        type: String,
-        required: [true, 'Please enter an email'],
-        lowercase: true,
-        validate: [validator.isEmail, 'Please enter a valid email'],
-      },
-      company: {
-        type: String,
-        required: [true, 'Please enter your organisation name'],
-      },
-      key: {
-        type: String,
-        unique: true,
-      },
-      testUrl: {
-        type: String,
-      },
-      Question: {
-        type: [
-          {
-            name: {
-              type: String,
-              required: [true, 'Please enter question name'],
-              unique: true,
-            },
-            statement: {
-              type: String,
-              required: [true, 'Please enter question description'],
-            },
-            constraints: {
-              type: String,
-              required: [true, 'Please enter constraints for the question'],
-            },
-            testcases: {
-              type: [
-                {
-                  input: {
-                    type: [String],
-                    required: [true, 'Please enter input for the test case'],
-                  },
-                  output: {
-                    type: [String],
-                    required: [true, 'Please enter output for the test case'],
-                  },
-                },
-              ],
-              validate: {
-                validator: function () {
-                  if (this.testcases.length === 0) return false;
-                  else return true;
-                },
-                message: 'Please enter a test case',
+  name: {
+    type: String,
+    required: [true, 'Please enter a Test name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    lowercase: true,
+    validate: [validator.isEmail, 'Please enter a valid email'],
+  },
+  company: {
+    type: String,
+    required: [true, 'Please enter your organisation name'],
+  },
+  key: {
+    type: String,
+    unique: true,
+  },
+  testUrl: {
+    type: String,
+  },
+  Question: {
+    type: [
+      {
+        name: {
+          type: String,
+          required: [true, 'Please enter question name'],
+          unique: true,
+        },
+        statement: {
+          type: String,
+          required: [true, 'Please enter question description'],
+        },
+        constraints: {
+          type: String,
+          required: [true, 'Please enter constraints for the question'],
+        },
+        testcases: {
+          type: [
+            {
+              input: {
+                type: [String],
+                required: [true, 'Please enter input for the test case'],
+              },
+              output: {
+                type: [String],
+                required: [true, 'Please enter output for the test case'],
               },
             },
+          ],
+          validate: {
+            validator: function () {
+              if (this.testcases.length === 0) return false;
+              else return true;
+            },
+            message: 'Please enter a test case',
           },
-        ],
-        validate: {
-          validator: function () {
-            if (this.Question.length === 0) return false;
-            else return true;
-          },
-          message: 'Please enter a question set',
         },
       },
-      duration: {
-        start: {
-          type: Date,
-          required: [true, 'Please enter test start time'],
-        },
-        end: {
-          type: Date,
-          required: [true, 'Please enter test end time'],
-        },
+    ],
+    validate: {
+      validator: function () {
+        if (this.Question.length === 0) return false;
+        else return true;
       },
-      createdAt: {
-        type: Date,
-        default: Date.now(),
-      },
+      message: 'Please enter a question set',
+    },
+  },
+  duration: {
+    start: {
+      type: Date,
+      required: [true, 'Please enter test start time'],
+    },
+    end: {
+      type: Date,
+      required: [true, 'Please enter test end time'],
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
+
 const Test = mongoose.model('Test', testSchema);
 module.exports = Test;
